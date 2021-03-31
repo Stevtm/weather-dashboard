d20dea623896f366ff4bb901b3f2ddd33653f1a4
Stevtm <steventmaddison@outlook.com>
2021-03-30 21:22:07 -0400

    add uv to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,32 @@
-// call the openweathermap api to get weather information
-var getCurrentWeather = function (location) {
+// ----- use openweathermap api to get weather information -----
+
+// function that gets latitude and longitude based on city input
+var getCoordinates = function (location) {
 	// format the api url
 	var url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=c06ce19a48a657fbd564f534f209828a`;
+
+	fetch(url)
+		.then(function (response) {
+			// request was successful
+			if (response.ok) {
+				response.json().then(function (data) {
+					var lat = data.coord.lat;
+					var lon = data.coord.lon;
+					getCurrentWeather(lat, lon);
+				});
+			} else {
+				alert(`Error: ${response.statusText}`);
+			}
+		})
+		.catch(function (error) {
+			alert("Unable to find weather data.");
+		});
+};
+
+// function that gets weather based on coordinates (lat & long)
+var getCurrentWeather = function (lat, lon) {
+	// format the api url
+	var url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily,minutely&appid=c06ce19a48a657fbd564f534f209828a`;
 
 	fetch(url)
 		.then(function (response) {
@@ -22,15 +47,17 @@ var getCurrentWeather = function (location) {
 
 // function to display the current weather information on the page
 var showCurrentWeather = function (data) {
-	// declare variables for all requiremed information from the weather object
-	var temp = Math.round((data.main.temp - 273.15) * 1000) / 1000;
-	var hum = Math.round(data.main.humidity * 1000) / 1000;
-	var wind = Math.round(data.wind.speed * 1000) / 1000;
+	// declare variables for all required information from the weather object
+	var temp = Math.round((data.current.temp - 273.15) * 1000) / 1000;
+	var hum = Math.round(data.current.humidity * 1000) / 1000;
+	var wind = Math.round(data.current.wind_speed * 1000) / 1000;
+	var UV = Math.round(data.current.uvi * 1000) / 1000;
 
 	// show information on the page
 	$("#currentTemp").text(` ${temp}`);
 	$("#currentHum").text(` ${hum}`);
 	$("#currentWind").text(` ${wind} `);
+	$("#currentUV").text(` ${UV}`);
 };
 
-getCurrentWeather("Hamilton");
+getCoordinates("Toronto");
